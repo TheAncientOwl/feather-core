aa876468976e1e7e0980d7a5914163484cffc6d2
TheAncientOwl <dele.alex.o.o@gmail.com>
2024-09-28 13:39:58 +0300

    Move economy interfaces to module package

--- src/main/java/mc/owls/valley/net/feathercore/api/core/IFeatherCoreProvider.java
+++ src/main/java/mc/owls/valley/net/feathercore/api/core/IFeatherCoreProvider.java
@@ -2,10 +2,10 @@ package mc.owls.valley.net.feathercore.api.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import mc.owls.valley.net.feathercore.api.module.interfaces.IEconomyProvider;
 import mc.owls.valley.net.feathercore.modules.configuration.interfaces.IConfigurationManager;
 import mc.owls.valley.net.feathercore.modules.data.mongodb.api.IDAOAccessor;
 import mc.owls.valley.net.feathercore.modules.data.players.interfaces.IPlayersData;
+import mc.owls.valley.net.feathercore.modules.economy.interfaces.IEconomyProvider;
 import mc.owls.valley.net.feathercore.modules.loot.chests.interfaces.ILootChestsModule;
 import mc.owls.valley.net.feathercore.modules.restricted.pvp.interfaces.IPvPManager;
 import mc.owls.valley.net.feathercore.modules.translation.components.TranslationManager;
