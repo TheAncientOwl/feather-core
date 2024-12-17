bf5909677fc553ec48de6610c455fd4a6749ceea
TheAncientOwl <dele.alex.o.o@gmail.com>
2024-12-17 16:22:55 +0200

    Update parser with new approach

--- src/main/java/mc/owls/valley/net/feathercore/modules/teleport/commands/TeleportLastLocationCommand.java
+++ src/main/java/mc/owls/valley/net/feathercore/modules/teleport/commands/TeleportLastLocationCommand.java
@@ -6,7 +6,7 @@
  *
  * @file TeleportLastPositionCommand.java
  * @author Alexandru Delegeanu
- * @version 0.8
+ * @version 0.9
  * @description Teleport to the last known location of the player
  */
 
@@ -21,6 +21,7 @@ import org.bukkit.entity.Player;
 
 import mc.owls.valley.net.feathercore.api.common.java.Pair;
 import mc.owls.valley.net.feathercore.api.common.language.Message;
+import mc.owls.valley.net.feathercore.api.common.minecraft.Args;
 import mc.owls.valley.net.feathercore.api.common.minecraft.Placeholder;
 import mc.owls.valley.net.feathercore.api.common.util.StringUtils;
 import mc.owls.valley.net.feathercore.api.core.FeatherCommand;
@@ -79,36 +80,55 @@ public class TeleportLastLocationCommand extends FeatherCommand<TeleportLastLoca
         }
     }
 
-    // TODO: Update with new parser approach
     protected CommandData parse(final CommandSender sender, final String[] args) {
         PlayerModel destination = null;
         Player who = null;
 
         switch (args.length) {
-            // /tpoffline [destination-player]
             case 1: {
-                destination = getInterface(IPlayersData.class).getPlayerModel(args[0]);
+                // /tpoffline [destination-player]
+                final var parsedArgs = Args.parse(args, Args::getOfflinePlayer);
 
-                if (!(sender instanceof Player)) {
-                    getLanguage().message(sender, Message.General.PLAYERS_ONLY);
-                    return null;
+                if (parsedArgs.success()) {
+                    if (!(sender instanceof Player)) {
+                        getLanguage().message(sender, Message.General.PLAYERS_ONLY);
+                        return null;
+                    } else {
+                        who = (Player) sender;
+                        destination = getInterface(IPlayersData.class).getPlayerModel(parsedArgs.getOfflinePlayer(0));
+                    }
                 } else {
-                    who = (Player) sender;
+                    getLanguage().message(sender, Message.General.NOT_VALID_PLAYER,
+                            Pair.of(Placeholder.STRING, args[0]));
+                    return null;
                 }
-
                 break;
             }
-            // /tpoffline [destination-player] (who-player)
             case 2: {
-                destination = getInterface(IPlayersData.class).getPlayerModel(args[0]);
+                // /tpoffline [destination-player] (who-player)
+                final var parsedArgs = Args.parse(args, Args::getOfflinePlayer, Args::getOnlinePlayer);
 
-                who = Bukkit.getPlayerExact(args[1]);
-                if (who == null) {
-                    getLanguage().message(sender, Message.General.NOT_ONLINE_PLAYER,
-                            Pair.of(Placeholder.PLAYER, args[1]));
+                if (parsedArgs.success()) {
+                    destination = getInterface(IPlayersData.class).getPlayerModel(parsedArgs.getOfflinePlayer(0));
+                    who = parsedArgs.getPlayer(1);
+                    if (who == null) {
+                        return null;
+                    }
+                } else {
+                    switch (parsedArgs.failIndex()) {
+                        case 0: {
+                            getLanguage().message(sender, Message.General.NOT_VALID_PLAYER,
+                                    Pair.of(Placeholder.STRING, args[0]));
+                            break;
+                        }
+                        case 1: {
+                            getLanguage().message(sender, Message.General.NOT_ONLINE_PLAYER,
+                                    Pair.of(Placeholder.PLAYER, args[1]));
+                            break;
+                        }
+                    }
                     return null;
                 }
-
                 break;
             }
             default: {
