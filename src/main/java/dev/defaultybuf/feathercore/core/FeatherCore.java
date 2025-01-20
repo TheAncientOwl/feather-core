10acc318331e9d829761f815c9c932724c08ca0b
TheAncientOwl <dele.alex.o.o@gmail.com>
2025-01-20 21:50:16 +0200

    Add entry plugin point to plugin core

---
+++ src/main/java/dev/defaultybuf/feathercore/core/FeatherCore.java
@@ -0,0 +1,18 @@
+/**
+ * ------------------------------------------------------------------------- *
+ *                     Copyright (c) by FeatherCore 2024                     *
+ * ------------------------------------------------------------------------- *
+ * @license https://github.com/TheAncientOwl/feather-core/blob/main/LICENSE
+ *
+ * @file FeatherCore.java
+ * @author Alexandru Delegeanu
+ * @version 0.1
+ * @description FeatherCore plugin entry point
+ */
+
+package dev.defaultybuf.feathercore.core;
+
+import dev.defaultybuf.feather.toolkit.core.FeatherPlugin;
+
+public class FeatherCore extends FeatherPlugin {
+}
